8b5407c514e1770f7be742809bf6ebd5227d44b2
Arthos <42878451+Arthurdoar@users.noreply.github.com>
2025-01-05 01:21:50 -0300

    Update content.js
    se agregó que busque identificador en hexadecimal de 32 caracteres

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-if (window.location.host !== "https://lectortmo.com/") {
-    let match = window.location.pathname.match(/\/news\/([a-fA-F0-9]+)\//); // /\/([^\/]+)\//
+if (window.location.host !== "https://zonatmo.com/") { 
+    let match = window.location.pathname.match(/\/news\/([a-fA-F0-9]{32})\//)    //se agregó que busque un identificador en hexadecimal de 32 caractéres
     if (match) {
         let id = match[1];
         let matchOptions = window.location.pathname.match(/\/\w+\/\w+\/(.*)/);
         if (matchOptions) {
-            window.location.replace("https://lectortmo.com/viewer/" + id + "/" + matchOptions[1])
-        } else window.location.replace("https://lectortmo.com/viewer/" + id)
+            window.location.replace("https://zonatmo.com/viewer/" + id + "/" + matchOptions[1])
+        } else window.location.replace("https://zonatmo.com/viewer/" + id)
     } else console.log("TempHexID not found")
 }
